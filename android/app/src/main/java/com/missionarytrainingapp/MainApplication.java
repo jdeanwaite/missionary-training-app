ae9e6244a94d1a34b1d1bab4cd5bb7fdeff4f43f
Justin <jdeanwaite@gmail.com>
2018-03-23 23:05:43 -0700

    Compiles now with react-native-fs

--- android/app/src/main/java/com/missionarytrainingapp/MainApplication.java
+++ android/app/src/main/java/com/missionarytrainingapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.missionarytrainingapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import org.reactnative.camera.RNCameraPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
             new ReactVideoPackage(),
             new VectorIconsPackage(),
             new RNCameraPackage()
